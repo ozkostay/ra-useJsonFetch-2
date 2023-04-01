68d621c77451996339cf92c78e6dcf09614c018d
ozkostay <ozkostay@mail.ru>
2023-04-01 15:00:58 +0500

    start backend down

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import List from './components/List';
+import Details from './components/Details'
+
+export default function App() {
+
+    return (
+        <div className='wrapper'>
+
+            {/* <List list={list} fnSelectItem={fnSelectItem}/>
+            <Details item={item}/> */}
+        </div>
+    );
+}
